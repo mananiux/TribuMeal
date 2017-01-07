98fe0c63a0992fb6b6d869e4c3dacd0da3eb5eca
mananiux <jlm2454@rit.edu>
2017-01-06 21:47:37 -0800

    Fine tuned the UI layouts to include upcoming meals and fixed the placement of the different layout.

--- app/src/main/java/com/tribumeal/activities/StorefrontActivityView.java
+++ app/src/main/java/com/tribumeal/activities/StorefrontActivityView.java
@@ -1,18 +1,17 @@
-package com.tribumeal;
+package com.tribumeal.activities;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
-public class StorefrontActivity extends AppCompatActivity {
+import com.tribumeal.R;
+
+public class StorefrontActivityView extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_storefront);
+        setContentView(R.layout.activity_storefront_view);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
